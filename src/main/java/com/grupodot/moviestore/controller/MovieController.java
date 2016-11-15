97f609d5f95806646e0ee3268329cb030fd64368
cmateusperez <cmateusperez@gmail.com>
2016-11-14 19:31:28 -0500

    Mejora paginas

--- src/main/java/com/grupodot/moviestore/controller/MovieController.java
+++ src/main/java/com/grupodot/moviestore/controller/MovieController.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseId;
 
@@ -95,6 +96,8 @@ public class MovieController{
 	 * Save movie
 	 */
 	public void saveMovie() {
+		FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Creación exitosa"));
 		movieService.saveMovie(movie);
 	}
 
